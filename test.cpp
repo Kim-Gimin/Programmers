98e1c346e70fecb0a207b7cfca0ad6d6d7f3b571
Kim-Gimin <kimjjang1997@gmail.com>
2024-05-28 15:14:13 +0900

    cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
 
 using namespace std;
+
+int solution(vector<string> strs, string t){
+    
+    set<string> str_set(strs.begin(), strs.end());
+    const int INF = 987654321;
+    int dp[20002];
+    int len = t.length();
+
+    for (int i = 0; i < len; i++){
+        dp[i] = INF;
+        }
+    dp[len] = 0;
+    for (int i = len -1; i>= 0; --i){
+        string tmp = "";
+        for(int j = 0; i + j < len && j < 5; ++j){
+            tmp += t[i+j];
+            if(str_set.find(tmp)!= str_set.end() &&dp[i+j+1] != INF){
+                dp[i] = min(dp[i], dp[i+j+1]+1);
+            }
+        }
+
+        
+        
+    }
+    if(dp[0] == INF){
+            return -1;}
+    return dp[0];    
+}
 int main(){
-cout << "hello world" << endl;
-return 0;
+    vector<string> strs = {"ba","na","n","a"};
+    string t = "banana";
+    cout << solution(strs, t) << endl;
+    return 0;
+
 }
--- 단어퍼즐.cpp
+++ 단어퍼즐.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-    cout << "hello";
-    return 0;
+int solutionI(vector<string> strs, string t){
+    
+    set<string> str_set(strs.begin(), strs.end());
+    const int INF = 987654321;
+    int dp[20002];
+    int len = t.length();
+
+    for (int i = 0; i < len; i++){
+        dp[i] = INF;
+        }
+    dp[len] = 0;
+    for (int i = len -1; i>= 0; --i){
+        string tmp = "";
+        for(int j = 0; i + j < len && j < 5; ++j){
+            tmp += t[i+j];
+            if(str_set.find(tmp)!= str_set.end() &&dp[i+j+1] != INF){
+                dp[i] = min(dp[i], dp[i+j+1]+1);
+            }
+        }
+
+        if(dp[0] == INF){
+            return -1;}
+        return dp[0];    
+    }
 }
